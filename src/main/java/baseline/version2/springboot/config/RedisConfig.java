8f1e1e71cc6aaa25a9c6d92bbf51acbcca8602cd
김병주 <atker14@gmail.com>
2023-11-23 09:47:31 +0900

    레디스 기본 명령 빈 등록

--- src/main/java/baseline/version2/springboot/config/RedisConfig.java
+++ src/main/java/baseline/version2/springboot/config/RedisConfig.java
@@ -10,7 +10,7 @@ import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.*;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
@@ -69,6 +69,26 @@ public class RedisConfig {
                 .fromConnectionFactory(redisConnectionFactory)
                 .cacheDefaults(redisCacheConfiguration)
                 .build();
+    }
+
+    @Bean
+    public HashOperations<?, Object, Object> hashOperations(RedisTemplate<?, ?> redisTemplate){
+        return redisTemplate.opsForHash();
+    }
+
+    @Bean
+    public ValueOperations<?, ?> valueOperations(RedisTemplate<?, ?> redisTemplate){
+        return redisTemplate.opsForValue();
+    }
+
+    @Bean
+    public ListOperations<?, ?> listOperations(RedisTemplate<?, ?> redisTemplate){
+        return redisTemplate.opsForList();
+    }
+
+    @Bean
+    public SetOperations<?, ?> setOperations(RedisTemplate<?, ?> redisTemplate){
+        return redisTemplate.opsForSet();
     }
 
 }
