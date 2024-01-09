d8a88199383c4f07e54afc06bf54f0ddbc1866a0
kbj <atker14@gmail.com>
2024-01-09 13:18:11 +0900

    controller 공통

--- src/main/java/baseline/version3/springboot/exceptionHandler/CustomExceptionHandler.java
+++ src/main/java/baseline/version3/springboot/exceptionHandler/CustomExceptionHandler.java
@@ -2,13 +2,25 @@ package baseline.version3.springboot.exceptionHandler;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Slf4j
 @ControllerAdvice
 public class CustomExceptionHandler {
+
+    /**
+     * 모든 컨트롤러 매핑 공통
+     */
+    @ModelAttribute
+    public void handleRequest(HttpServletRequest request, Model model) {
+        String requestURI = request.getRequestURI();
+
+        log.info("[URI] {}", requestURI);
+    }
 
     @ExceptionHandler(RuntimeException.class)
     protected String exception(RuntimeException exception,
--- src/main/java/baseline/version3/springboot/exceptionHandler/CustomRestExceptionHandler.java
+++ src/main/java/baseline/version3/springboot/exceptionHandler/CustomRestExceptionHandler.java
@@ -2,10 +2,13 @@ package baseline.version3.springboot.exceptionHandler;
 
 import baseline.version3.springboot.common.util.ResponseUtil;
 import baseline.version3.springboot.exceptionHandler.exception.ServiceLayerException;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
@@ -14,6 +17,16 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class CustomRestExceptionHandler {
 
     private ResponseUtil responseUtil;
+
+    /**
+     * 모든 Rest 컨트롤러 매핑 공통
+     */
+    @ModelAttribute
+    public void handleRequest(HttpServletRequest request, Model model) {
+        String requestURI = request.getRequestURI();
+
+        log.info("[API] {}", requestURI);
+    }
 
     @ExceptionHandler(ServiceLayerException.class)
     protected ResponseEntity<?> serviceLayerException(ServiceLayerException serviceLayerException){
